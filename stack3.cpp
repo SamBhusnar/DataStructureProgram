a33a0632afd76d3e7d3f2e1096b313a9dea9a450
sam <samadhanbhusnar21@gmail.com>
2022-09-30 14:27:56 +0530

    .\stack3.cpp is created now

--- stack3.cpp
+++ stack3.cpp
@@ -1,11 +1,67 @@
 // Ranjit Deshmukh and Samadhan Bhusnar
 // reverse entered string using stack
 #include <iostream>
+#include <string.h>
 using namespace std;
+#define max 20
+class stack
+{
+public:
+    int item[max], top;
+    void create(stack *);
+    int pop(stack *);
+    void push(stack *, int);
+};
+void stack::create(stack *p)
+{
+    cout << "Stack is created\n";
+    p->top = -1;
+}
+
+void stack::push(stack *p, int element)
+{
+    if (p->top == max - 1)
+    {
+        cout << "stack overflows...";
+    }
+    else
+    {
+        ++p->top;
+        p->item[p->top] = element;
+    }
+}
+int stack::pop(stack *p)
+{
+    if (p->top == -1)
+    {
+        cout << "stack underflow...";
+    }
+    else
+    {
+        return (p->item[p->top--]);
+    }
+}
 int main()
 {
-
-    cout << "i will write some code here\n";
-
+    char str[20], rev[20], ch;
+    int i = 0, j = 0;
+    stack *p, q, obj;
+    p = &q;
+    obj.create(p);
+    cout << "Enter any string \n";
+    cin >> str;
+    while (str[i] != '\0')
+    {
+        obj.push(p, str[i]);
+        i++;
+    }
+    while (p->top != -1)
+    {
+        ch = obj.pop(p);
+        rev[j] = ch;
+        j++;
+    }
+    rev[j]='\0';
+    cout << rev;
     return 0;
 }
