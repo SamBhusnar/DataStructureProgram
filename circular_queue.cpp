66969a3f901869fedc8b018518ee9dbcf0df1a2d
sam <samadhanbhusnar21@gmail.com>
2022-10-01 14:39:02 +0530

    samadhan bhusnar commit now

---
+++ circular_queue.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+#define max 5
+class Queue
+{
+public:
+    int item[max], rear, front;
+    void create(Queue *q);
+    void isfull(Queue *);
+    void isempty(Queue *);
+    void insert(Queue *, int);
+    int remove(Queue *);
+    void display(Queue *);
+};
+void create(Queue *q)
+{
+    q->front = -1;
+    q->rear = -1;
+    cout << "Circular Queue is create\n";
+}
+void Queue::isfull(Queue *q)
+{
+    if ((q->front == 0 && q->rear == max - 1) || (q->rear + 1 == q->front))
+    {
+        cout << "Circular queue is full\n";
+    }
+    else
+    {
+        cout << " Circular queue is not full\n";
+    }
+}
+void Queue::isempty(Queue *q)
+{
+    if (q->front == -1)
+    {
+        cout << "Circular Queue is empty";
+    }
+    else
+    {
+        cout << "Circular Queus is not empty";
+    }
+}
+void Queue::insert(Queue *q, int element)
+{
+    if (q->rear == max - 1)
+    {
+        cout << "Queue is overflow...";
+    }
+    else
+    {
+        ++q->rear;
+        q->item[q->rear] = element;
+    }
+}
+int Queue::remove(Queue *q)
+{
+    if (q->rear == q->front)
+    {
+        cout << "Queue is underflow...";
+    }
+    else
+    {
+        ++q->front;
+        cout << q->item[q->front] << " is element is removed \n";
+        return (q->item[q->front]);
+    }
+}
+void Queue::display(Queue *q)
+{
+    int i;
+    // int save = q->front;
+    // // q->front = -1; // this statement introduce  an bug
+    // for (i = q->rear; i > save; i--)
+    // {
+    //     ++q->front;
+    //     cout << q->item[q->front] << '\n';
+    // }
+    // q->front = save;
+    for (i = q->front + 1; i <= q->rear; i++)
+    {
+        cout << "\n"
+             << q->item[i];
+    }
+}
+int main()
+{
+    int inp, elem, rmelement;
+    Queue obj, *q, p;
+    q = &p;
+    do
+    {
+        cout << "\n1.create\n2.isfull\n3.isempty\n4.insert\n5.remove\n6.display\n7.exit\n";
+        cin >> inp;
+        switch (inp)
+        {
+        case 1:
+            obj.create(q);
+            break;
+        case 2:
+            obj.isfull(q);
+            break;
+        case 3:
+            obj.isempty(q);
+            break;
+        case 4:
+            cout << "Enter element to insert in queue\n";
+            cin >> elem;
+            obj.insert(q, elem);
+            break;
+        case 5:
+            rmelement = obj.remove(q);
+            break;
+        case 6:
+            obj.display(q);
+            break;
+
+        case 7:
+            exit(1);
+            break;
+        }
+    } while (inp != 7);
+
+    return 0;
+}
--- stack4.cpp
+++ stack4.cpp
@@ -1,7 +1,99 @@
-#include<iostream>
+#include <windows.h>
+#include <iostream>
+#include <string.h>
 using namespace std;
-int main(){
-cout<<"good afternoon!\n";
+#define max 50
+class stack
+{
+public:
+    int item[max], top;
+    void create(stack *);
+    int pop(stack *);
+    void push(stack *, int);
+};
+void stack::create(stack *p)
+{
+    cout << "Stack is created\n";
+    p->top = -1;
+}
+void stack::push(stack *p, int element)
+{
+    if (p->top == max - 1)
+    {
+        cout << "stack overflows...";
+    }
+    else
+    {
+        ++p->top;
+        p->item[p->top] = element;
+    }
+}
+int stack::pop(stack *p)
+{
+    if (p->top == -1)
+    {
+        cout << "stack underflow...";
+    }
+    else
+    {
+        return (p->item[p->top--]);
+    }
+}
+int main()
+{
+    stack *ops, *oprnd, a, b, obj;
+    char ch;
+    ops = &a;
+    oprnd = &b;
+    obj.create(ops);
+    obj.create(oprnd);
+    int i = 0, j = 0;
+    char infix[50], prefix[50];
+    cout << "Enter any infix expression \n";
+    cin >> infix;
 
-return 0;
+    strrev(infix);
+    cout << infix;
+    Sleep(10000);
+    while (infix[i] != '\0')
+    {
+        if (infix[i] == 'a' || infix[i] == 'b' || infix[i] == 'c' || infix[i] == 'd' || infix[i] == 'z')
+        {
+            obj.push(oprnd, infix[i]);
+        }
+        else if (infix[i] == '+' || infix[i] == '-' || infix[i] == '*' || infix[i] == '/' || infix[i] == ')')
+        {
+            obj.push(ops, infix[i]);
+        }
+        else
+        {
+            while (ops->item[ops->top] != -1)
+            {
+                ch = obj.pop(ops);
+                if (ch != ')') // one para or ')' this also
+                {
+                    obj.push(oprnd, ch);
+                }
+            }
+        }
+
+        i++;
+    }
+    if (ops->top != -1)
+    {
+        ch = obj.pop(ops);
+        if (ch != '(')
+        {
+            obj.push(oprnd, ch);
+        }
+    }
+
+    while (oprnd->top != -1)
+    {
+        prefix[j] = obj.pop(oprnd);
+        j++;
+    }
+    prefix[j] = '\0';
+    cout << prefix;
+    return 0;
 }
